9dd85bdbd95ca6eeca23194469ca8e044a3a0cf3
Luis Fernandez <luis.fernandez@avantica.net>
2015-06-05 12:04:58 -0600

    Create CoachUs.App.Web project

---
+++ CoachUs.App.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoachUs.App.Web.MvcApplication" Language="C#" %>
---
+++ CoachUs.App.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoachUs.App.Web.Startup))]
+namespace CoachUs.App.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
