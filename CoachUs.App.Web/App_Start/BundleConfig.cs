fecb51d435b8a2f1ff36dbd5e8756a24a4d8d7fc
Luis Fernandez <luis.fernandez@avantica.net>
2015-06-08 10:57:22 -0600

    Tuning up bootstrap tempate to use resources from project path

--- CoachUs.App.Web/App_Start/BundleConfig.cs
+++ CoachUs.App.Web/App_Start/BundleConfig.cs
@@ -23,8 +23,46 @@ namespace CoachUs.App.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new ScriptBundle("~/bundles/more-jquery").Include(
+                        "~/Scripts/jquery-ui.custom.js",
+                        "~/Scripts/jquery.ui.touch-punch.js",
+                        "~/Scripts/jquery.easypiechart.js",
+                        "~/Scripts/jquery.sparkline.js",
+                        "~/Scripts/flot/jquery.flot.js",
+                        "~/Scripts/flot/jquery.flot.pie.js",
+                        "~/Scripts/flot/jquery.flot.resize.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ace").Include(
+                        "~/Scripts/ace/elements.scroller.js",
+                        "~/Scripts/ace/elements.colorpicker.js",
+                        "~/Scripts/ace/elements.fileinput.js",
+                        "~/Scripts/ace/elements.typeahead.js",
+                        "~/Scripts/ace/elements.wysiwyg.js",
+                        "~/Scripts/ace/elements.spinner.js",
+                        "~/Scripts/ace/elements.treeview.js",
+                        "~/Scripts/ace/elements.wizard.js",
+                        "~/Scripts/ace/elements.aside.js",
+                        "~/Scripts/ace/ace.js",
+                        "~/Scripts/ace/ace.ajax-content.js",
+                        "~/Scripts/ace/ace.touch-drag.js",
+                        "~/Scripts/ace/ace.sidebar.js",
+                        "~/Scripts/ace/ace.sidebar-scroll-1.js",
+                        "~/Scripts/ace/ace.submenu-hover.js",
+                        "~/Scripts/ace/ace.widget-box.js",
+                        "~/Scripts/ace/ace.settings.js",
+                        "~/Scripts/ace/ace.settings-rtl.js",
+                        "~/Scripts/ace/ace.settings-skin.js",
+                        "~/Scripts/ace/ace.widget-on-reload.js",
+                        "~/Scripts/ace/ace.searchbox-autocomplete.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ace-extra").Include(
+                      "~/Scripts/ace-extra.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap-css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/css/font-awesome.css"));
+
+            bundles.Add(new StyleBundle("~/Content/site").Include(
                       "~/Content/site.css"));
         }
     }
